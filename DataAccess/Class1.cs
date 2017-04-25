1649c02aca53144eb9b30b2b97b4ead67170ab82
Artur Ziemiański <aziemianski@hqsoftware.pl>
2017-04-25 22:38:18 +0200

    Empty projects

---
+++ DataAccess/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Contact.DataAccess
+{
+    public class Class1
+    {
+    }
+}
---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Contact.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
